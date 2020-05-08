2e3d53545f14ccd4cc25bfa29ec77645801a6b75
Dominik Buszewski <yogiasdasd@gmail.com>
2020-05-08 09:27:20 +0200

    changes in contact page

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -3,29 +3,32 @@ import "./contact.styles.scss";
 // import { ReactComponent as FacebookIcon } from "../../assets/icons/facebook.svg";
 import { ReactComponent as LinkedinIcon } from "../../assets/icons/linkedin.svg";
 import { ReactComponent as GithubIcon } from "../..//assets/icons/github.svg";
-import { Link } from "react-router-dom";
+import { ReactComponent as EmailMessageIcon } from "../../assets/icons/email.svg";
 
 const ContactPage = () => (
 	<div className="conntact-page" id="contact">
 		<h1>CONTACT</h1>
-		<h3>Write to my on my email</h3>
-		<p>buszewski91@gmail.com</p>
-		<h3>or find me on my social media</h3>
+
 		<div className="social-media">
+			<p>Email: buszewski91@gmail.com</p>
+
 			<div className="icon-container">
 				<a
 					href="https://pl.linkedin.com/in/dominik-buszewski-4697171a4"
 					target="_blank"
 				>
 					<LinkedinIcon className="contact-icon" />
+					<p>Linkedin</p>
 				</a>
 			</div>
 			<div className="icon-container">
 				<a href="https://github.com/DominikBuszewski" target="_blank">
 					<GithubIcon className="contact-icon" />
+					<p>Github</p>
 				</a>
 			</div>
 		</div>
+
 		<footer className="footer">
 			<p>© Dominik Buszewski, 2020.</p>
 		</footer>
