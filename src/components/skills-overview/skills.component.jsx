33043711c6630da30f6c63229fecf8f43434e88b
Dominik Buszewski <yogiasdasd@gmail.com>
2020-05-19 11:10:56 +0200

    skills update

--- src/components/skills-overview/skills.component.jsx
+++ src/components/skills-overview/skills.component.jsx
@@ -9,18 +9,19 @@ const SkillsOverview = () => (
 				<p>HTML</p>
 				<p>CSS</p>
 				<p>SCSS</p>
+				<p>Styled Components</p>
 				<p>JAVASCRIPT</p>
 				<p>REACT</p>
 				<p>REACT ROUTER</p>
 				<p>REDUX</p>
 				<p>REACT HOOKS</p>
-				<p>Context API</p>
-				<p>YARN</p>
+				<p>YARN/NPM</p>
 				<p>GIT</p>
+				<p>JEST</p>
 				<p>FIREBASE</p>
 				<h2>Next to learn</h2>
 				<p>TYPESCRIPT</p>
-				<p>JEST</p>
+				<p>GraphQL</p>
 				<p>NODEJS</p>
 				<p>MANGODB</p>
 			</div>
